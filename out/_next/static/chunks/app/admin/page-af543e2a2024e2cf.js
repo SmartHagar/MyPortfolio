bea9423e4f63cd81ef4840a3b3543a33c7d5ada8
SmartHagar <smarthagar@gmail.com>
2023-10-19 20:00:55 +0900

    mengganti posisi navigation

--- out/_next/static/chunks/app/admin/page-af543e2a2024e2cf.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3],{5139:function(e,r,t){Promise.resolve().then(t.bind(t,6247))},6247:function(e,r,t){"use strict";t.r(r);var n=t(9268);t(6006),r.default=function(e){let{}=e;return(0,n.jsx)("div",{children:"Page"})}},3177:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(6006),o=Symbol.for("react.element"),f=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),u=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,s={key:!0,ref:!0,__self:!0,__source:!0};function i(e,r,t){var n,i={},_=null,c=null;for(n in void 0!==t&&(_=""+t),void 0!==r.key&&(_=""+r.key),void 0!==r.ref&&(c=r.ref),r)f.call(r,n)&&!s.hasOwnProperty(n)&&(i[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===i[n]&&(i[n]=r[n]);return{$$typeof:o,type:e,key:_,ref:c,props:i,_owner:u.current}}r.jsx=i,r.jsxs=i},9268:function(e,r,t){"use strict";e.exports=t(3177)}},function(e){e.O(0,[667,488,744],function(){return e(e.s=5139)}),_N_E=e.O()}]);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,27 +12,27 @@ import Nav from "@/components/navbar/Nav";
 export default function Home() {
   return (
     <div className="lg:flex bg-black-100 text-body">
-      <div className="fixed flex justify-center w-full lg:w-auto z-50">
+      <div className="fixed bottom-0 flex justify-center w-full lg:fixed lg:w-auto lg:bottom-auto z-50">
         <Nav />
       </div>
       <div className="flex-1 px-10 lg:pl-48 lg:mr-80 lg:pr-4">
         <Element name="dashboard">
           <Dashboard />
         </Element>
-        <Element name="tentang" className="pt-20">
+        <Element name="tentang" className="pb-20 pt-4 lg:pt-20 lg:pb-0">
           <Tentang />
         </Element>
-        <Element name="kemampuan" className="pt-20">
+        <Element name="kemampuan" className="pb-20 pt-4 lg:pt-20 lg:pb-0">
           <Kemampuan />
         </Element>
-        <Element name="projects" className="pt-20">
+        <Element name="projects" className="pb-20 pt-4 lg:pt-20 lg:pb-0">
           <Projects />
         </Element>
-        <Element name="my-kontak" className="pt-20">
+        <Element name="my-kontak" className="pb-20 pt-4 lg:pt-20 lg:pb-0">
           <MyKontak />
         </Element>
       </div>
-      <div className=" xl:block xl:fixed lg:right-0 lg:w-80 lg:h-screen lg:bg-black-200">
+      <div className="xl:block xl:fixed lg:right-0 lg:w-80 lg:h-screen lg:bg-black-200">
         <Profile />
       </div>
     </div>
